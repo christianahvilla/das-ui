f9037061a0b29d5e249ac58dc705179b59d664aa
Christian Herrejon <v-cherrejon@expediagroup.com>
2021-12-27 13:53:57 -0600

    Update Progress Props

--- src/stories/Progress/CommonProgress.jsx
+++ src/stories/Progress/CommonProgress.jsx
@@ -1,25 +1,31 @@
-import { string } from 'prop-types';
+import { bool, string } from 'prop-types';
 import React from 'react';
 import Loader from 'rsuite/Loader';
 
 const CommonProgress = (props) => {
     const {
+        backdrop,
+        center,
         content,
         size,
         speed,
     } = props;
     return (
-        <Loader content={content} size={size} speed={speed} />
+        <Loader backdrop={backdrop} center={center} content={content} size={size} speed={speed} />
     );
 };
 
 CommonProgress.defaultProps = {
+    backdrop: false,
+    center: false,
     content: 'Loading...',
     size: 'md',
     speed: 'fast',
 };
 
 CommonProgress.propTypes = {
+    backdrop: bool,
+    center: bool,
     content: string,
     size: string,
     speed: string,
--- src/stories/Progress/CommonProgress.stories.js
+++ src/stories/Progress/CommonProgress.stories.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { select, text } from '@storybook/addon-knobs';
+import { boolean, select, text } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react';
 import CommonProgress from './CommonProgress';
 import { COMPONENTS_SIZES, PROGRESS_SPEEDS } from '../../helpers/types';
 
 storiesOf('Progress', module).add('Progress', () => (
     <CommonProgress
+        backdrop={boolean('Backdrop', false)}
+        center={boolean('Center', false)}
         content={text('Content', 'Default Loading...')}
-        size={select('size', COMPONENTS_SIZES, COMPONENTS_SIZES[0])}
-        speed={select('speed', PROGRESS_SPEEDS, PROGRESS_SPEEDS[0])}
+        size={select('Size', COMPONENTS_SIZES, COMPONENTS_SIZES[0])}
+        speed={select('Speed', PROGRESS_SPEEDS, PROGRESS_SPEEDS[0])}
     />
 ));
